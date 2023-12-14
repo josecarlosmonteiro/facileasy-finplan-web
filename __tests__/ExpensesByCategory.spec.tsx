1bfc8a62ecf66ce41bb36776f8cb13b0adf35128
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2023-12-14 12:09:33 -0300

    feat: configurar testes unitários + primeiro teste de componente.

---
+++ __tests__/ExpensesByCategory.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+
+import { ExpensesByCategory } from '@/components/releases/fixed/ExpensesByCategory';
+import { TRelease } from '@/types/releases';
+
+const mock: TRelease[] = [
+  { id: 1, category: 'x', title: '', transferType: '', type: 'out', value: 100 },
+  { id: 2, category: 'x', title: '', transferType: '', type: 'out', value: 200 },
+  { id: 3, category: 'y', title: '', transferType: '', type: 'out', value: 100 },
+]
+
+describe("components/releases/fixed/ExpensesByCategory", () => {
+  test("renderizar componente", () => {
+    render(<ExpensesByCategory expenses={[]} />);
+
+    const title = screen.getByText(/despesas por categoria/i);
+
+    expect(title).toBeInTheDocument();
+  })
+})
