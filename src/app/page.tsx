0e8efbf71a3a017a0ef8251d46ac19231f9e2d60
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2024-01-14 22:32:24 -0300

    feat: implementar página de login + página dashboard (sem dados).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,31 @@
+import Link from "next/link";
+
 function currency(value: string | number) {
   return Number(value).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 }
 
 export default function Home() {
   return (
-    <main>
-      <div className="p-4">
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-7">
-            <div className="flex justify-center items-center gap-4">
-              <div className="w-full h-full p-4 py-2 rounded shadow-md shadow-gray-100">
-                <div className="flex justify-between align-items text-emerald-500">
-                  <h2>Receitas</h2>
-                  <div>{currency(4200)}</div>
-                </div>
-              </div>
-              <div className="w-full h-full p-4 rounded shadow-md shadow-gray-100">
-                <div className="flex justify-between align-items text-blue-500">
-                  <h2>Balanço</h2>
-                  <div>{currency(3200)}</div>
-                </div>
-              </div>
-              <div className="w-full h-full p-4 py-2 rounded shadow-md shadow-gray-100">
-                <div className="flex justify-between align-items text-red-500">
-                  <h2>Despesas</h2>
-                  <div>{currency(3200)}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-span-2">
-            salve...
-          </div>
+    <main className="h-screen grid grid-cols-2 gap-4 items-center">
+      <div className="h-full text-center p-4 bg-blue-800 text-white">
+        <h1 className="text-3xl">[TITLE]</h1>
+        <br />
+        <h2 className="text-xl">[SUBTITLE]</h2>
+        <br />
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatum vel totam, consectetur impedit minima earum dolore facilis. Deserunt blanditiis cum ducimus amet totam sequi, culpa ullam voluptatibus fugit. Non.
+        </p>
+        <br />
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, iusto voluptatem asperiores eius architecto adipisci quos est delectus cum consectetur voluptatum eos quia blanditiis et distinctio ullam nostrum at accusantium.
+        </p>
+      </div>
+
+      <div className="h-full flex items-center justify-center">
+        <div className="p-4 rounded shadow-md bg-gray-100">
+          <Link href={'/inicio'} className="text-xl text-blue-400">Entrar na conta</Link>
         </div>
       </div>
     </main>
